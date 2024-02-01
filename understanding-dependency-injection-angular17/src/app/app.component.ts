2eae4ee34521a70b88e6b6b7b4ac8ae6bc334541
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-02-01 14:34:34 +0500

    Inverse of Singleton service by providing service to Component decorator of AppComponent class

--- understanding-dependency-injection-angular17/src/app/app.component.ts
+++ understanding-dependency-injection-angular17/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { BaseRepository } from './services/BaseRepository/BaseRepository';
+import { BaseRepositoryImpl3 } from './services/BaseRepositoryImpl3/BaseRepositoryImpl3';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, LoginComponent],
+  providers: [{ provide: BaseRepository, useClass: BaseRepositoryImpl3 }],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- understanding-dependency-injection-angular17/src/app/app.config.ts
+++ understanding-dependency-injection-angular17/src/app/app.config.ts
@@ -2,11 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { BaseRepository } from './services/BaseRepository/BaseRepository';
-import { BaseRepositoryImpl1 } from './services/BaseRepositoryImpl1/BaseRepositoryImpl1';
-import { BaseRepositoryImpl2 } from './services/BaseRepositoryImpl2/BaseRepositoryImpl2';
-import { BaseRepositoryImpl3 } from './services/BaseRepositoryImpl3/BaseRepositoryImpl3';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), { provide: BaseRepository, useClass: BaseRepositoryImpl3 }]
+  providers: [provideRouter(routes)]
 };
