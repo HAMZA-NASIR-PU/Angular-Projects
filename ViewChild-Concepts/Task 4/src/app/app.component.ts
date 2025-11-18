d8546c351b05f5576e0a641d6a1e9b164e262637
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2025-11-18 19:02:04 +0500

    Modification in Task 4 of ViewChild concepts

--- ViewChild-Concepts/Task 4/src/app/app.component.ts
+++ ViewChild-Concepts/Task 4/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule, NgForOf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 import {
   Component,
@@ -11,7 +11,7 @@ import {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgForOf],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
